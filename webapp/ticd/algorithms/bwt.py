e5b910184d0500fe2dbda6e4d0ad3aaa032e1926
Francesco Saverio Cannizzaro <francescosaveriocannizzaro@gmail.com>
2018-04-22 16:43:09 +0200

    add bwt, fibonacci

---
+++ webapp/ticd/algorithms/bwt.py
@@ -0,0 +1,65 @@
+from collections import deque
+from functools import reduce
+
+
+def suffix_array(text):
+    sa = [text[i:] for i in range(len(text))]
+    ordered = {word: [] for word in set(sa)}
+    for pos, word in enumerate(sa):
+        ordered[word].append(pos)
+    return reduce(list.__add__, map(lambda x: x[1], sorted(ordered.items())))
+
+
+def encode(text, use_suffix_array=False):
+    """
+    >>> encode('mississippi', True)
+    ('ipssmhpissii', 'h')
+
+    :param text:
+    :param use_suffix_array:
+    :return:
+    """
+    text += chr(ord(min(text)) - 1)
+
+    if use_suffix_array:
+        sa = suffix_array(text)
+        return ''.join([text[sa[i] - 1] if sa[i] > 0 else text[-1] for i in range(len(text))]), text[-1]
+
+    rotations = []
+    start = deque(text)
+
+    for _ in range(len(text)):
+        start.rotate(1)
+        rotations.append(''.join(start))
+
+    return ''.join(map(lambda rot: rot[-1], sorted(rotations))), text[-1]
+
+
+def decode(l):
+    """
+    >>> decode('ipssmhpissii')
+    'mississippi'
+
+    :param l:
+    :return:
+    """
+    t = l.index(min(l))
+    f = sorted(l)
+    w = f[t]
+    tao = {char: [] for char in set(f)}
+    for pos, char in enumerate(l):
+        tao[char].append(pos)
+    tao = reduce(list.__add__, map(lambda x: x[1], sorted(tao.items())))
+    for _ in range(1, len(l)):
+        t = tao[t]
+        w += f[t]
+    return w[:-1]
+
+
+if __name__ == '__main__':
+    to_encode = 'mississippi'
+    encoded_sa, eos = encode(to_encode, True)
+    encoded_basic, _ = encode(to_encode)
+    print(' >> encoding', to_encode, 'to [suffix-array]', encoded_sa, '[basic]', encoded_basic, ', $ =', eos)
+    decoded = decode(encoded_sa)
+    print(' >> decoding', encoded_sa, 'to ', decoded)
---
+++ webapp/ticd/algorithms/fibonacci.py
@@ -0,0 +1,54 @@
+sequences = [0, 1]
+
+# initial computation
+
+for i in range(100):
+    sequences.append(sequences[-1] + sequences[-2])
+
+
+def encode(integers):
+    """
+    >>> encode([5, 5, 5, 8, 13])
+    '0001100011000110000110000011'
+
+    :param integers:
+    :return:
+    """
+    symbols = {}
+
+    for integer in set(integers):
+        tmp = integer
+        fns = []
+
+        while tmp > 0:
+            value = max(filter(lambda x: x <= tmp, sequences))
+            fns.append(sequences.index(value) - 2)
+            tmp -= value
+
+        symbols[integer] = ''.join(['1' if x in fns else '0' for x in range(max(fns) + 1)]) + '1'
+
+    return ''.join([symbols[integer] for integer in integers])
+
+
+def decode(text):
+    """
+    >>> decode('0001100011000110000110000011')
+    [5, 5, 5, 8, 13]
+
+    :param text:
+    :return:
+    """
+    integers = map(lambda x: x + '1', filter(lambda x: x, text.split('11')))
+    return [sum([sequences[x + 2] for x, value in enumerate(integer) if value == '1']) for integer in integers]
+
+
+if __name__ == '__main__':
+    import time
+
+    start_time = time.clock()
+    to_encode = [5, 5, 5, 8, 13]
+    encoded = encode(to_encode)
+    print(" >> encoding", to_encode, " to ", encoded)
+    decoded = decode(encoded)
+    print(" >> decoding", encoded, 'to', decoded)
+    print("execution time", time.clock() - start_time, "seconds")
