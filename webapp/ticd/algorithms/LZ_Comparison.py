d1a797bea055bb31281541f49844f5b5357429cb
Mirko Avantaggiato <mirko.avantaggiato@gmail.com>
2018-05-02 15:50:44 +0200

    typo

--- webapp/ticd/algorithms/LZ_Comparison.py
+++ webapp/ticd/algorithms/LZ_Comparison.py
@@ -7,25 +7,24 @@ from .Lyndon import DeBruijnSequence
 
 
 def main():
-    alphabet_size = 3
+    alphabet_size = 2
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     lengths = []
     lz77_lengths = []
     lz78_lengths = []
-    for lenght in range(2, 20):
-        lengths.append(lenght)
-        word = DeBruijnSequence(alphabet_size, lenght)
+    for length in range(2, 20):
+        lengths.append(length)
+        word = DeBruijnSequence(alphabet_size, length)
         word = "".join([alphabet[word[i]] for i in range(len(word))])
         lz77word = lz77_c(word)
         lz78word = lz78_c(word)
         lz77_lengths.append(len(lz77word))
         lz78_lengths.append(len(lz78word))
 
-
     fig, ax = plt.subplots()
     ax.plot(lengths, lz77_lengths, color='red', label="lz77")
     ax.plot(lengths, lz78_lengths, color='blue', label="lz78")
-    ax.set(xlabel='lenght', ylabel='LZ7*', title='')
+    ax.set(xlabel='length', ylabel='LZ7*', title='')
     ax.grid()
 
     plt.show()
