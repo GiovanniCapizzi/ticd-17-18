7bca08af763f1510ad69958e50d05c9e2de0b63b
Mirko Avantaggiato <mirko.avantaggiato@gmail.com>
2018-05-07 20:15:29 +0200

    to fix

--- webapp/ticd/algorithms/__init__.py
+++ webapp/ticd/algorithms/__init__.py
@@ -1,1 +1,19 @@
 # coding=utf-8
+import sys
+from . import unary_coding
+
+tmp = dir(sys.modules[__name__])
+algorithms_list = {
+    'GZIP': None,
+    'LZW': None,
+    'Unary': unary_coding,
+    'Gamma': None,
+    'Delta': None,
+    'Fibonacci': None,
+    'Levenshtein': None,
+    'RLE': None
+}
+algorithms = {
+    'LZ Family': ['GZIP', 'LZW'],
+    'Integers': ['Unary', 'Gamma', 'Delta', 'Fibonacci', 'Levenshtein ', 'RLE']
+}
--- webapp/ticd/views.py
+++ webapp/ticd/views.py
@@ -1,4 +1,17 @@
 # coding=utf-8
+
 from django.shortcuts import render
 
-# Create your views here.
+from .algorithms import algorithms_list
+
+
+def algorithm_view(request, algorithm):
+    return render(request, 'index.html', {
+        'algorithm': algorithm,
+        'algorithms': algorithms_list,
+        'input': [
+            # key, title, description, input type
+            ('input', 'Input', 'Inserisci la stringa da codificare', 'string'),
+            ('dimension', 'Dimensione Finestra', 'Inserisci qui la dimensione della finestra', 'bool')
+        ]
+    })
