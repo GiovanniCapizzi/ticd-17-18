544eb57b27bb1e8563b4415c0c1b92c0b4090cf8
Francesco Saverio Cannizzaro <francescosaveriocannizzaro@gmail.com>
2018-05-07 20:45:09 +0200

    clear

--- webapp/ticd/algorithms/__init__.py
+++ webapp/ticd/algorithms/__init__.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-import sys
 from os import path, listdir
 from . import unary_coding
 import importlib
@@ -7,8 +6,6 @@ import importlib
 excluded = ['compiled_libraries', 'utils.py', 'divina_commedia.txt', 'pytest.ini', 'test_file.py']
 
 files = [file for file in listdir(path.dirname(__file__)) if '__' not in file and file not in excluded]
-
-# modules = [importlib.import_module('.' + file[:-3], package='ticd.algorithms') for file in files]
 
 modules = {file[:-3]: importlib.import_module('.' + file[:-3], package='ticd.algorithms') for file in files}
 
