a7e6b12b7342afcbd3136b1bd30d077119a510bd
Mirko Avantaggiato <mirko.avantaggiato@gmail.com>
2018-05-12 00:47:06 +0200

    RLE.py

--- webapp/ticd/algorithms/RLE.py
+++ webapp/ticd/algorithms/RLE.py
@@ -2,13 +2,13 @@
 from typing import List, Tuple
 
 
-def rle(s: str) -> List[Tuple[str, int]]:
+def rle(input_string: str) -> List[Tuple[str, int]]:
     """
     >>> rle("aaaaaaaaaabbbbb")
     [('a', 10), ('b', 5)]
     """
     result: List[Tuple[str, int]] = []
-    as_list = list(s)
+    as_list = list(input_string)
     counter = 0
     prev = as_list[0]
     while len(as_list) != 0:
@@ -35,11 +35,11 @@ def rle_i(encoded: List[Tuple[str, int]]) -> str:
     return "".join(result)
 
 
-def rho(s: str) -> int:
+def rho(input_string: str) -> int:
     """
     >>> assert rho("aaaaaaaaaabbbbb") == 2
     """
-    return len(rle(s))
+    return len(rle(input_string))
 
 
 def main():
