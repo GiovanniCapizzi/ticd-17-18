a801b0a679aaa9c846d1b248459ada139269c31e
Francesco Saverio Cannizzaro <francescosaveriocannizzaro@gmail.com>
2018-05-12 16:36:13 +0200

    fix codebook

--- webapp/ticd/algorithms/LZW.py
+++ webapp/ticd/algorithms/LZW.py
@@ -19,18 +19,18 @@ __algorithm__ = "LZW"
 __group__ = "LZ"
 
 
-def encode(i_text: str, logging: bool = False) -> Dict:
+def encode(text: str, logging: bool = False) -> Dict:
     """
     >>> encode("bcababbcbcbaaaabbc")
     ([2, 3, 1, 2, 6, 4, 9, 1, 11, 8, 3], {1: 'a', 2: 'b', 3: 'c'})
 
-    :param i_text:
+    :param text:
     :param logging:
     :return output:
     """
 
     # Initialization -----------------------
-    in_alph = sorted(set(i_text))
+    in_alph = sorted(set(text))
     codebook = {}
     inverted_codebook = {}
     for index, item in enumerate(in_alph):
@@ -39,16 +39,16 @@ def encode(i_text: str, logging: bool = False) -> Dict:
 
     # Position
     cursor = 0
-    limit = len(i_text)
+    limit = len(text)
     output = []
 
     if logging:
-        print("CODING...\n", i_text)
+        print("CODING...\n", text)
 
     # End of Initialization -----------------
 
     while cursor < limit:
-        current = i_text[cursor]
+        current = text[cursor]
 
         if logging:
             print("The current word is", current, 'at index', cursor)
@@ -56,22 +56,22 @@ def encode(i_text: str, logging: bool = False) -> Dict:
         # searching the longest word in the dictionary
         relative_index = cursor + 1
 
-        while i_text[cursor: relative_index] in codebook:
+        while text[cursor: relative_index] in codebook:
             relative_index += 1
             if relative_index > limit:
                 # end of string, outputting remaining text,
                 # relative_index bound doesn't matter in python substring
-                coding_word = i_text[cursor:relative_index]
+                coding_word = text[cursor:relative_index]
                 output.append(codebook[coding_word])
                 if logging:
                     print("and outputting the remaining", codebook[coding_word], "value")
-                return {'result': ''.join([str(out) for out in output]), 'codebook': inverted_codebook}
+                return {'result': output, 'codebook': inverted_codebook}
 
         # add this new word to the codebook
-        word = i_text[cursor:relative_index]
+        word = text[cursor:relative_index]
         codebook[word] = len(codebook) + 1
         # outputting value
-        coding_word = i_text[cursor:relative_index - 1]
+        coding_word = text[cursor:relative_index - 1]
         output.append(codebook[coding_word])
 
         if logging:
@@ -82,18 +82,19 @@ def encode(i_text: str, logging: bool = False) -> Dict:
         cursor += len(coding_word)
 
 
-def decode(i_coded_text: str, codebook: Dict, logging: bool = False) -> str:
+def decode(encoded: List[int], codebook: Dict, logging: bool = False) -> str:
     """
     >>> decode([2, 3, 1, 2, 6, 4, 9, 1, 11, 8, 3], {1: 'a', 2: 'b', 3: 'c'})
     'bcababbcbcbaaaabbc'
 
-    :param i_coded_text:
+    :param encoded:
     :param codebook:
     :param logging:
     :return output:
     """
-
     # Initialization -----------------------
+
+    codebook = {int(key): value for key, value in codebook.items()}
 
     output = ''
     # last word added
@@ -101,7 +102,7 @@ def decode(i_coded_text: str, codebook: Dict, logging: bool = False) -> str:
 
     # End of Initialization -----------------
 
-    for item in i_coded_text:
+    for item in encoded:
         if item in codebook:
             current_item = codebook[item]
             output += current_item
