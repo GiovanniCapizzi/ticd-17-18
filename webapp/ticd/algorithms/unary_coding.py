ca81e8fb33a675bb0fb47e80901b15658c818e0a
Mirko Avantaggiato <mirko.avantaggiato@gmail.com>
2018-05-23 21:27:21 +0200

    input_example unary_coding.py

--- webapp/ticd/algorithms/unary_coding.py
+++ webapp/ticd/algorithms/unary_coding.py
@@ -1,10 +1,13 @@
 # coding=utf-8
 from typing import List
 
+from .utils import input_example
+
 __algorithm__ = "Unary"
 __group__ = "integers"
 
 
+@input_example(list_of_integers="46 33 13 1 48 23 34 13 15 3")
 def encode_unary(list_of_integers: List[int]) -> str:
     """
     >>> encode_unary([13, 13])
@@ -17,9 +20,11 @@ def encode_unary(list_of_integers: List[int]) -> str:
     return "".join(["0" * (n - 1) + "1" for n in list_of_integers])
 
 
+@input_example(
+        list_of_integers="01000000000000000100000010000000000001")
 def decode_unary(binary_string: str) -> List[int]:
     """
-    >>> decode_unary('00000000000010000000000001')
+    >>> decode_unary('00000000000010000000000000001')
     [13, 13]
     """
     as_list = list(binary_string)
