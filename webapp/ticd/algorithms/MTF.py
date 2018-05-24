ee10754b056194fb3d081a7223c0f6af4fbfdc83
Mirko Avantaggiato <mirko.avantaggiato@gmail.com>
2018-05-24 13:17:06 +0200

    MTF.py input_example

--- webapp/ticd/algorithms/MTF.py
+++ webapp/ticd/algorithms/MTF.py
@@ -1,13 +1,14 @@
 # coding=utf-8
-import random
-import string
-from typing import List
+from typing import Any, Dict, List
+
+from .utils import input_example
 
 __algorithm__ = "Move To Front"
 __group__ = "miscellaneous"
 
 
-def encode_mtf(input_string: str) -> List[int]:
+@input_example(input_string='hello')
+def encode_mtf(input_string: str) -> Dict[str][Any]:
     L = input_string
     X = sorted(set(L))
     output = []
@@ -16,10 +17,20 @@ def encode_mtf(input_string: str) -> List[int]:
         X.remove(c)
         X.insert(0, c)
 
-    return output
+    return {
+        "output": output,
+        "alphabet": " ".join(
+                list(
+                        map(lambda x: str(ord(x)), sorted(set(input_string)))
+                )
+        )
+    }
 
 
+@input_example(integer_list='1 1 2 0 3',
+               alphabet='101 104 108 111')
 def decode_mtf(integer_list: List[int], alphabet: List[str]) -> str:
+    alphabet = list(map(chr, [int(i) for i in alphabet]))
     x = alphabet
     tmp: List[str] = [""] * len(integer_list)
     for i in range(0, len(tmp)):
@@ -31,17 +42,8 @@ def decode_mtf(integer_list: List[int], alphabet: List[str]) -> str:
     return "".join(tmp)
 
 
-def test():
-    for _ in range(100):
-        w = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(128))
-        alphabet = sorted(set(w))
-        encoded = encode_mtf(w)
-        decoded = decode_mtf(encoded, alphabet)
-        assert w == decoded
-
-
 def main():
-    test()
+    pass
 
 
 if __name__ == '__main__':
