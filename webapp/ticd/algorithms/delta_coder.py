27dfb59b4bb0bbd268fe11a4f3cf8cde1fd386e8
giuseppefilippone <filipponegiuseppe94@gmail.com>
2018-06-05 18:32:10 +0200

    fix delta coder

--- webapp/ticd/algorithms/delta_coder.py
+++ webapp/ticd/algorithms/delta_coder.py
@@ -4,7 +4,7 @@
 from math import log2
 from typing import List, Dict
 
-from .gamma import encode as gamma_encode
+from .gamma import encode as gamma_encode, decode as gamma_decode
 from .utils import input_example, integers_encode, integers_decode
 
 __author__ = "Giuseppe Filippone"
@@ -17,56 +17,35 @@ class DeltaCoder:
     def __init__(self):
         pass
 
-    def encode(self, integer):
+    def __encode(self, integer):
         return gamma_encode([int(log2(integer)) + 1]) + bin(integer)[3:]
 
-    def encode_sequence(self, integers):
+    def encode(self, integers):
         encoded = ""
         for integer in integers:
-            encoded += self.encode(integer)
+            encoded += self.__encode(integer)
         return encoded
 
-    def m_encode(self, integers):
-        codewords = {}
-        for integer in integers:
-            codewords[integer] = self.encode(integer)
-        return codewords
-
-    def decode(self, codeword):
-        N = 0
-        for c in codeword:
-            if c == "0":
-                N += 1
-            else:
-                break
-        return int("0b1" + codeword[N + N + 1:], 2)
-
-    def decode_text(self, text):
+    def decode(self, text):
         i = 0
         decoded = []
         while i < len(text):
-            N = 0
+            j, N = i, 0
             while i < len(text) and text[i] == "0":
                 N += 1
                 i += 1
             tmp = i + N + 1
-            N = int("0b" + text[i:tmp], 2) - 1 if len(text[i:tmp]) > 0 else 0
+            N = gamma_decode(text[j:tmp])[0] - 1 if len(text[j:tmp]) > 0 else 0
             decoded.append(int("0b1" + text[tmp:tmp + N], 2))
             i = tmp + N
         return decoded
 
-    def m_decode(self, codewords):
-        plainwords = {}
-        for codeword in codewords:
-            plainwords[codeword] = self.decode(codeword)
-        return plainwords
 
-
-@input_example(integers="21 1 1 2 4 10")
+@input_example(integers="21 -1 1 -2 -4 10")
 def encode(integers: List[int]) -> Dict[str, str]:
-    return {"text": DeltaCoder().encode_sequence(integers_encode(integers))}
+    return {"text": DeltaCoder().encode(integers_encode(integers))}
 
 
 @input_example(text="0010101011101000110000100010")
 def decode(text: str) -> List[int]:
-    return integers_decode(DeltaCoder().decode_text(text))
+    return integers_decode(DeltaCoder().decode(text))
